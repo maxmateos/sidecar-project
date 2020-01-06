db7cdcf75b7a50c1e0914e59ab978e66b79891fd
Max Mateos <maximiliano.mateos@disqo.com>
2020-01-06 15:04:51 -0800

    [DAUDFES-1] Create initial commit for jump start projects - Created basic structure for clean architecture pattern - Added Dockerization - Added Sonarqube

--- core/src/main/java/com/disqo/springbootquickstart/repository/ExampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.disqo.springbootquickstart.repository;
-
-import com.disqo.springbootquickstart.entity.Example;
-
-public interface ExampleRepository {
-    Example getExampleData(Long param1, String param2);
-}
